480fd3fb0087056c28205a11bc9078a0bda5dc83
Alberto Leal <albertonb@gmail.com>
2014-12-09 12:48:48 -0200

    Add README and script to create the package.

--- Formula/backstage.rb
+++ Formula/backstage.rb
@@ -2,12 +2,13 @@ require 'formula'
 
 class Backstage < Formula
   homepage 'https://github.com/backstage'
-  url 'https://github.com/backstage/backstage-client/archive/0.0.1.tar.gz'
-  sha1 'd16f7d228b4ddf5ddd1bd976543b754b9766f5bc'
+  url 'https://github.com/backstage/backstage-client/releases/download/0.0.1/backstage-client-0.0.1.tar.gz'
+  sha256 '44f294277c5dbad0d0fde79a3ddc076bc046bebc07fcf27c503f5baa7bc53744'
 
   depends_on 'go'
 
   def install
+    mkdir_p "src/github.com/backstage/backstage-client"
     system "bash", "-c", "GOPATH=\"$PWD\" go build -o backstage github.com/backstage/backstage-client/backstage"
     bin.install "backstage"
   end
---
+++ README.md
@@ -0,0 +1,13 @@
+#backstage-client
+
+This repository was based on the one created by [Tsuru team](https://github.com/tsuru/homebrew-tsuru).
+
+##Usage
+
+First, add this tap:
+
+	% brew tap backstage/homebrew-backstage
+
+Then install the desired formula:
+
+	% brew install backstage
