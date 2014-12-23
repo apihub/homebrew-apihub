71ccfcd17d199eec3f6fecbe640e99c2e628f736
Alberto Leal <albertonb@gmail.com>
2014-12-23 10:44:04 -0200

    0.0.6

--- Formula/backstage.rb
+++ Formula/backstage.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Backstage < Formula
   homepage 'https://github.com/backstage'
-  url 'https://github.com/backstage/backstage-client/releases/download/0.0.5/backstage-client-0.0.5'
-  sha256 '9bfc75ada81a675f7e68514f0e95fcda90dbcf83385dd879f1ee5aef6300a7be'
+  url 'https://github.com/backstage/backstage-client/releases/download/0.0.6/backstage-client-0.0.6'
+  sha256 '73286d92fd069d0592fefb283fa916460200bfa700c4db501aab2329bfbc4d72'
 
   depends_on 'go'
 
