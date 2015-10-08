37195f1fab1f6779b743eca48efdc128cadb9175
Alberto Leal <albertonb@gmail.com>
2015-10-08 13:42:38 -0300

    Rename the file name.

--- Formula/apihub.rb
+++ Formula/apihub.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class ApiHub < Formula
+class Apihub < Formula
   homepage 'https://github.com/apihub'
   url 'https://github.com/apihub/apihub-cli/releases/download/0.0.10/apihub-cli-0.0.10'
   sha256 '65dd489b084b8f64681f192c01e8dc1e6ae6cd83d7654be582a0b3b85738bb52'
