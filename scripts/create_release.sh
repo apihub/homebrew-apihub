68c666225818d795994666a4e624b788822509c9
Alberto Leal <albertonb@gmail.com>
2015-10-08 11:06:17 -0300

    Change project name

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-# This script builds and uploads backstage's client to Github.
+# This script builds and uploads apihub's client to Github.
 #
 #   You need to export one environment variable called GITHUB_TOKEN.
 #   You should be able to create one token at https://github.com/settings/applications
@@ -13,27 +13,27 @@ current_path=`pwd`
 destination_dir="/tmp/dist-src"
 
 function download {
-    echo -n "Downloading backstage-client source... "
-    mkdir -p /tmp/backstage-client/src /tmp/backstage-client/pkg
-    GOPATH=/tmp/backstage-client go get -d github.com/backstage/backstage-client/...
-    pushd /tmp/backstage-client/src/github.com/backstage/backstage-client > /dev/null 2>&1
+    echo -n "Downloading apihub-cli source... "
+    mkdir -p /tmp/apihub-cli/src /tmp/apihub-cli/pkg
+    GOPATH=/tmp/apihub-cli go get -d github.com/apihub/apihub-cli/...
+    pushd /tmp/apihub-cli/src/github.com/apihub/apihub-cli > /dev/null 2>&1
     git checkout master >/dev/null 2>&1
     echo "ok"
     echo -n "Restoring dependencies... "
-    GOPATH=/tmp/backstage-client godep restore ./...
+    GOPATH=/tmp/apihub-cli godep restore ./...
     popd > /dev/null 2>&1
     echo "ok"
 }
 
 function get_version {
-    GOPATH=/tmp/backstage-client go build -o backstage github.com/backstage/backstage-client/backstage
-    echo $(./backstage --version | awk '{print $3}' | sed -e 's/\.$//')
-    rm backstage
+    GOPATH=/tmp/apihub-cli go build -o apihub github.com/apihub/apihub-cli/apihub
+    echo $(./apihub --version | awk '{print $3}' | sed -e 's/\.$//')
+    rm apihub
 }
 
 function package {
     echo -n "Creating package... "
-    pushd /tmp/backstage-client
+    pushd /tmp/apihub-cli
     tar -czf $1 *
     shasum -a 256 $1
     popd
@@ -44,9 +44,9 @@ function update_formula {
     echo -n "Updating and tagging formula..."
     file_path=${destination_dir}/$1
     sha=$(shasum -a 256 ${file_path}|awk -F' ' '{ print $1}')
-    github_link=$(echo "https://github.com/backstage/backstage-client/releases/download/$2/$1" | sed 's/\//\\\//g'| awk -F ".tar.gz" '{print $1}')
-    sed -i "" "s/url '.*'/url '${github_link}'/g" ${current_path}/../Formula/backstage.rb
-    sed -i "" "s/sha256 '.*'/sha256 '${sha}'/g" ${current_path}/../Formula/backstage.rb
+    github_link=$(echo "https://github.com/apihub/apihub-cli/releases/download/$2/$1" | sed 's/\//\\\//g'| awk -F ".tar.gz" '{print $1}')
+    sed -i "" "s/url '.*'/url '${github_link}'/g" ${current_path}/../Formula/apihub.rb
+    sed -i "" "s/sha256 '.*'/sha256 '${sha}'/g" ${current_path}/../Formula/apihub.rb
     echo "ok"
 }
 
@@ -56,18 +56,18 @@ echo "ok"
 
 download
 client_version=$(get_version)
-package ${destination_dir}/backstage-client-${client_version}.tar.gz
+package ${destination_dir}/apihub-cli-${client_version}.tar.gz
 
 cd ${destination_dir}
 
 echo -n "Uploading file to Github... "
-github-release release --security-token $GITHUB_TOKEN --user backstage --repo backstage-client --tag ${client_version} --pre-release
-github-release upload --security-token $GITHUB_TOKEN --user backstage --repo backstage-client --tag ${client_version} --name backstage-client-${client_version} --file backstage-client-${client_version}.tar.gz
+github-release release --security-token $GITHUB_TOKEN --user apihub --repo apihub-cli --tag ${client_version} --pre-release
+github-release upload --security-token $GITHUB_TOKEN --user apihub --repo apihub-cli --tag ${client_version} --name apihub-cli-${client_version} --file apihub-cli-${client_version}.tar.gz
 echo "ok"
-update_formula backstage-client-${client_version}.tar.gz ${client_version}
+update_formula apihub-cli-${client_version}.tar.gz ${client_version}
 
 echo -n "Removing temporary folder..."
-rm -rf /tmp/backstage-client
+rm -rf /tmp/apihub-cli
 echo "ok"
 
 echo -n "Generating tag and commiting..."
